41ccc2c4f7196db570397bada29ca9799f4fab2c
Ignacio Avas <gmail@ignacioavas.com>
2014-09-13 22:34:54 -0300

    Export AAB functions

--- RenderEngine/math/AAB.h
+++ RenderEngine/math/AAB.h
@@ -7,17 +7,18 @@
 #pragma once
 #include "Vector3.h"
 #include "math/Sphere.h"
+#include "render_engine_export_api.h"
 
 class AAB
 {
 public:
     Vector3 min;
     Vector3 max;
-    AAB(const Vector3 &, const Vector3 &);
-    AAB();
-    Vector3 getExtent() const;
-    Vector3 getCenter() const;
-    Sphere getBoundingSphere() const;
+    RENDER_ENGINE_EXPORT_API AAB(const Vector3 &, const Vector3 &);
+    RENDER_ENGINE_EXPORT_API AAB();
+    RENDER_ENGINE_EXPORT_API Vector3 getExtent() const;
+    RENDER_ENGINE_EXPORT_API Vector3 getCenter() const;
+    RENDER_ENGINE_EXPORT_API Sphere getBoundingSphere() const;
     void addPadding(float a)
     {
         min -= a;
