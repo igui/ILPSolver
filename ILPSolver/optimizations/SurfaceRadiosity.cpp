7f83b7f140b840a9af62d53215c1c512e9d63c74
Ignacio Avas <gmail@ignacioavas.com>
2014-11-24 01:06:20 -0200

    Clarifies radius calculation

--- ILPSolver/optimizations/SurfaceRadiosity.cpp
+++ ILPSolver/optimizations/SurfaceRadiosity.cpp
@@ -29,35 +29,30 @@ SurfaceRadiosity::SurfaceRadiosity(Logger *logger, PMOptixRenderer *renderer, Sc
 
 Evaluation *SurfaceRadiosity::evaluateRadiosity()
 {
-	logger->log("Evaluating solution (With radiosity)\n");
 	renderer->render(defaultPhotonWidth, sampleImageHeight, sampleImageWidth, *sampleCamera, true);
 	return genEvaluation();
 }
 
 Evaluation *SurfaceRadiosity::evaluateFast()
 {
-	logger->log("Evaluating solution (Fast)\n");
 	renderer->genPhotonMap(defaultPhotonWidth);
 	return genEvaluation();
 }
 
 Evaluation *SurfaceRadiosity::genEvaluation()
 {
-	unsigned int n  = renderer->getHitCount().at(objectId);
-	float p = (float) n / renderer->totalPhotons();
-	float W = renderer->getRadiance().at(objectId) / surfaceArea;
-
-	/*
-		// for calculating minimun photonWidth
-		static const float 4z2 = 7.6832f;
-		float minimumNumPhotons = 4z2 * (1-p) * p * (r/confidenceIntervalRadius)*(r/confidenceIntervalRadius);
-		int minimumWidth = ceilf(sqrtf(minimumNumPhotons));
-		qDebug() << ("min width: " + QString::number(minimumWidth));
-	*/
-
 	const static float z = 1.96f;
-	float radius = z * W * sqrtf(p*(1-p)/n);
-	return new SurfaceRadiosityEvaluation(W, radius);
+
+	// n is the hit count at the surface
+	unsigned int n  = renderer->getHitCount().at(objectId);
+	// p is the probability estimate n / (total hits on surfaces)
+	float p = (float) n / renderer->totalPhotons();
+	// r is the surface radiosity estimate
+	float r = renderer->getRadiance().at(objectId) / surfaceArea;
+	// radius is the confidence radius given by equations 
+	float radius =  z * r * sqrtf(p*(1-p)/n);
+
+	return new SurfaceRadiosityEvaluation(r, radius);
 }
 
 class SurfaceRadiosityImageSaveASyncTask : public QRunnable
