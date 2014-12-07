6612782f2b88c19cb2e3056a82162c4ced0c4ffb
Ignacio Avas <gmail@ignacioavas.com>
2014-12-07 21:39:00 -0200

    Enhanced Evaluation Definition

--- ILPSolver/optimizations/Evaluation.cpp
+++ ILPSolver/optimizations/Evaluation.cpp
@@ -9,3 +9,19 @@ Evaluation::Evaluation(void)
 Evaluation::~Evaluation(void)
 {
 }
+
+QString toString(EvaluationResult::CompareResult compareResult)
+{
+	switch(compareResult){
+	case EvaluationResult::BETTER:
+		return "Better";
+	case EvaluationResult::WORSE:
+		return "Worse";
+	case EvaluationResult::SIMILAR:
+		return "Similar";
+	case EvaluationResult::EQUAL:
+		return "Equal";
+	default:
+		throw std::invalid_argument("Unknown argument");
+	}
+}
--- ILPSolver/optimizations/Evaluation.h
+++ ILPSolver/optimizations/Evaluation.h
@@ -7,11 +7,14 @@ namespace EvaluationResult
 	enum CompareResult { BETTER, WORSE, EQUAL, SIMILAR };
 };
 
+QString toString(EvaluationResult::CompareResult compareResult);
+
 class Evaluation
 {
 public:
 	virtual EvaluationResult::CompareResult compare(const Evaluation* other) const = 0;
-	virtual operator QString() const = 0;
+	virtual QString info() const = 0;
+	virtual QString infoShort() const = 0;
 	virtual ~Evaluation(void);
 protected:
 	Evaluation(void);
