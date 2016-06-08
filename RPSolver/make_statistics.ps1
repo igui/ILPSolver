724d197d4a6d85b640cdbe47d00223603b28ac1e
Ignacio Avas <gmail@ignacioavas.com>
2016-06-08 09:25:48 -0300

    Statistics script now gets the log file as well

--- RPSolver/make_statistics.ps1
+++ RPSolver/make_statistics.ps1
@@ -1,10 +1,11 @@
-$scenes = 'cornell-move-cone', 'sponza-hole', 'conference-simple';
+$scenes = 'cornell-move-cone', 'sponza-2-holes', 'conference-simple';
 $rpsolver = '..\x64\Debug\RPSolver.exe';
 $triesPerScene = 10;
 
 foreach ($scene in $scenes) {
 	for($i=1; $i -le $triesPerScene; $i++){
-		$outLogFile = ".\examples\$scene-$i-log.txt";
+		$outLogFile = ".\examples\$scene-log-$i.txt";
+		$outSolutionsFile = ".\examples\$scene-solutions-$i.csv";
 
 		if(Test-Path $outLogFile){
 			Write-Host $scene $i "Omitted";
@@ -14,6 +15,7 @@ foreach ($scene in $scenes) {
 		Write-Host $scene $i;
 		Invoke-Expression "& $rpsolver .\examples\$scene.xml";
 		Move-Item ".\examples\log.txt" $outLogFile;
+		Move-Item ".\examples\output\solutions.csv" $outSolutionsFile;
 		Write-Host "Cooling down";
 		Start-Sleep -s 10;
 	}
